9a5a68c51517590d7f068831a317de8a378c1029
Litsu9999 <102814127+Litsu9999@users.noreply.github.com>
2022-06-23 02:25:27 +0200

    Update GCIController.java

--- src/controller/GCIController.java
+++ src/controller/GCIController.java
@@ -1,9 +1,31 @@
 package controller;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
 /**
  * Class controller pour la page GCI.fxml
  * @author Theo 
- * @version 1.0
+ * @version 1.1
  */
 public class GCIController extends NavigationControls {
     private static final int[] NBMOIS = {31,28,31,30,31,30,31,31,30,31,30,31};
